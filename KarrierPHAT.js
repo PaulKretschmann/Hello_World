e582b53404b82650dcd8abbb03b3a8822a22e33d
Paul <paulk26@gmx.de>
2024-06-28 14:21:40 +0200

    versuch buttons

--- KarrierPHAT.js
+++ KarrierPHAT.js
@@ -1,1 +1,17 @@
+function showContent() {
+    var content = document.getElementById("content");
+    var showButton = document.getElementById("showButton");
+    var hideButton=document.getElementById("hideButton")
+    content.style.display="block";
+    showButton.style.display = 'none'; 
+    hideButton.style.display = 'block'; 
+}
+function hideContent() {
+    var content = document.getElementById('content');
+    var showButton = document.getElementById('showButton');
+    var hideButton = document.getElementById('hideButton');
 
+    content.style.display = 'none'; 
+    showButton.style.display = 'block'; 
+    hideButton.style.display = 'none'; 
+}
